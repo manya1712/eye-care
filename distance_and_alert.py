d2b6781468660b2d5099bbf987f75814e484d455
Harmanjit Singh <harmanjit140500@gmail.com>
2022-01-22 22:26:10 +0530

    distance done

---
+++ distance_and_alert.py
@@ -0,0 +1,46 @@
+import cv2
+import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
+import pyautogui as pag
+import screen_brightness_control as sbc
+
+
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector()
+alert = 0
+
+
+while True:
+    success, img = cap.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    if faces:
+        face = faces[0]
+
+        left_pupil = face[145]
+        right_pupil = face[374]
+
+        w, _info, _image = detector.findDistance(
+            left_pupil, right_pupil, img)
+
+        W = 6.3
+
+        f = 600
+        D = W*f/w
+
+        cvzone.putTextRect(
+            img, f'Distance {int(D)} cm', (face[10][0]-100, face[10][1]-20), 2, 3)
+
+        if D < 30:
+            if(alert >= 5):
+                pag.alert(text="You are very close to your Display, Doctors recommend to stay atleast 50 cms away from your monitor to avoid eye-strain, We are dimming your monitor's brightness If you are still to close to your monitor", title="Distance Alert")
+                alert = 3
+                sbc.set_brightness(0)
+            else:
+                pag.alert(text="You are very close to your Display, Doctors recommend to stay atleast 50 cms away from your monitor to avoid eye-strain", title="Distance Alert")
+                alert += 1
+                
+
+    # cv2.imshow("Image", img)
+    cv2.waitKey(1)
